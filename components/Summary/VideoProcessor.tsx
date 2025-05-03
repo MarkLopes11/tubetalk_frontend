c8eeed5ed684a83046c4200ca99db6410e9195b4
Mark Lopes <mark157898@gmail.com>
2025-05-03 08:57:44 +0530

    ngrok

--- components/Summary/VideoProcessor.tsx
+++ components/Summary/VideoProcessor.tsx
@@ -32,7 +32,7 @@ export default function VideoProcessor({ onProcessComplete }) {
 
     try {
       // Process video first
-      const processResponse = await fetch("http://localhost:8000/process_video/", {
+      const processResponse = await fetch("https://9036-115-245-117-34.ngrok-free.app/process_video/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ video_url: videoUrl }),
